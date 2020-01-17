84e5cf601e01aaa7ed73c311fac8920dad8bd385
Gabriel Anguiano <anguianog97@gmail.com>
2020-01-17 08:45:26 -0800

    project routes working believe its completed

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectRoutes = require('./data/routes/projectRoutes')
-const actionRoutes = require('./data/routes/actionRoutes')
+
 const server = express();
 
 server.use(express.json())
 
 
 server.use('/api/project', projectRoutes);
-server.use('/api/action', actionRoutes)
 
 server.use(helmet());
 
